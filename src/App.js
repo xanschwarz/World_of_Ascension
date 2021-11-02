bd0ab7efada2ffafef56682c76612757fca5d377
Xander <xanschwarz@gmail.com>
2021-11-01 22:28:16 -0700

    Set up React App.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>This is going to be our Project 3.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
