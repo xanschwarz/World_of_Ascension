801a2359ae8b83ddf72c144b538f5e992fad94b4
Xander <xanschwarz@gmail.com>
2021-11-16 20:05:13 -0800

    Initial visual changes to store.

--- client/src/pages/Store/Store.js
+++ client/src/pages/Store/Store.js
@@ -1,21 +1,29 @@
 // Once the buttons are working, make it so once you buy an item the buy button changes to an upgrade button.
 
 import React from 'react';
-import ring from './ring.PNG';
-import robe from './robe.png';
 import Auth from '../../utils/auth';
 import ModalContainer from '../../components/Modal/ModalContainer';
+
+const ringSRCTier1 =
+  'https://bn1303files.storage.live.com/y4mW_O9K00YJzIbF-y8GckCr7qpnqZ6-TlPWhfXNDX1pWrpEcNkEI1jiMdTq1gTeg8mmZWFgmezdGVnV4tyJiMk-0ojBNSYOSx9_SyKxqbMWMqdHLezvj1DO0NlRMEjKFDjS2gHElyD3iQReigcPbeZuyz5qMSSfuklfVBnjYgHiFBEjiRWjDuQA4Yubs5AP0753zYdEBYaREhn08zQNXRt4Q/10_t.PNG?psid=1&width=175&height=175&cropMode=center';
+const ringSRCTier2 =
+  'https://bn1303files.storage.live.com/y4m_-cwHk39j7JYzYc8ma1mUhLw3MboUUwGEh0FJ5MHf1WSVGXcIFOa3-asJFtom5QYtW6fLNlX1GOx4px22WCvtDwz6fc7I-_Aj2FrrVqBJNxF2PeESK7vfF-GB5rvfc3JZbnN4VM0eVcHQTX5nEHsNUBeVHts_4bwXSn0wG6_FE3WQgMxzBvlKtKDW11XQu5MScd1b2WViTmfvSi8QkeP8w/77_t.PNG?psid=1&width=256&height=256&cropMode=center';
+
+const cloakSRCTier1 =
+  'https://bn1303files.storage.live.com/y4m7NkuUdV2Afgv2gXJGVW3tsjT7FD6nxuz1bkGTaL4nGcQvPai_QjMqd0vcBBOkGNT7A5CPR4Vne_NDaCrBI507nSQydtxVY_oDA12LMHkn5kq5irODHAqgz0JfEJkjwkebhU98LOgaP6tm0yQEXgUtS0YkXlpeOK7aeUPhJtBR1iOqxrjMh2hit7PbR5l1nxUNxckKP4qDgDPf9a2OOMOlA/cloaks_4.png?psid=1&width=175&height=175&cropMode=center';
+const cloakSRCTier2 =
+  'https://bn1303files.storage.live.com/y4mY-GHuM_KNjzq5qsL0LV1UVYgvUTwk7sbG7jhxhA0f0J-4wll8IekvlTxW8ws9Oh2K0QUg3_7CvtXC0MhPGI-ymCXB7oIYL-VjqKSeVO0e9GQO8euEDJIDKYPwItIZlXtR-YaeVXC9T5rJ99xxllTT6KCBBgslToQnRa_HJZGK7lc-n3SUUXePB-3NKkM7Rs3lUuGeA5kQWo27lGJA1_RpQ/cloaks_14.png?psid=1&width=256&height=256&cropMode=center';
 
 const Store = () => {
   let riches = 0;
   let ringTier = 0;
-  let robeTier = 0;
+  let cloakTier = 0;
   // const richesBtn = document.getElementById('addRiches');
   // const macguffinsBtn = document.getElementById('MacGuffins');
   // const buyRingBtn = document.getElementById('buyRing');
   // const upgradeRingBtn = document.getElementById('upgradeRing');
-  // const buyRobeBtn = document.getElementById('buyRobe');
-  // const upgradeRobeBtn = document.getElementById('upgradeRobe');
+  // const buyCloakBtn = document.getElementById('buyCloak');
+  // const upgradeCloakBtn = document.getElementById('upgradeCloak');
 
   function addCurrency() {
     riches += 100;
@@ -37,18 +45,18 @@ const Store = () => {
     console.log('You upgraded your ring! It is now tier ' + ringTier);
   }
 
-  function buyRobe() {
+  function buyCloak() {
     riches -= 200;
     document.getElementById('MacGuffins').innerHTML = riches;
-    console.log('You bought a robe!');
+    console.log('You bought a cloak!');
   }
 
-  function upgradeRobe() {
+  function upgradeCloak() {
     riches -= 20;
-    robeTier += 1;
+    cloakTier += 1;
     document.getElementById('MacGuffins').innerHTML = riches;
-    document.getElementById('robeTierDisplay').innerHTML = robeTier;
-    console.log('You upgraded your robe! It is now tier ' + robeTier);
+    document.getElementById('cloakTierDisplay').innerHTML = cloakTier;
+    console.log('You upgraded your cloak! It is now tier ' + cloakTier);
   }
 
   return (
@@ -72,18 +80,19 @@ const Store = () => {
             <div className="space-y-12">
               <div className="space-y-5 sm:space-y-4 md:max-w-xl lg:max-w-3xl xl:max-w-none">
                 <h2 className="text-3xl font-extrabold text-white tracking-tight sm:text-4xl">
-                  Buy and Upgrade Your Gear
+                  Buy Rings and Cloaks to Upgrade Your Gear
                 </h2>
               </div>
-              <ul
-                // role="list"
-                className="space-y-4 sm:grid sm:grid-cols-2 sm:gap-6 sm:space-y-0 lg:grid-cols-3 lg:gap-8"
-              >
+              <ul className="space-y-4 sm:grid sm:grid-cols-2 sm:gap-6 sm:space-y-0 lg:grid-cols-3 lg:gap-8">
+                {/* Ring Tier 1 */}
                 <li className="py-10 px-6 bg-gray-800 text-center rounded-lg xl:px-10 xl:text-left">
                   <div className="space-y-6 xl:space-y-10">
+                    <h4 className="text-white text-center font-medium text-xl m-4">
+                      Tier 1 Ring
+                    </h4>
                     <img
                       className="mx-auto h-40 w-40 rounded-full xl:w-56 xl:h-56"
-                      src={ring}
+                      src={ringSRCTier1}
                       alt=""
                     ></img>
                     <div className="space-y-2 xl:flex xl:items-center xl:justify-between">
@@ -94,59 +103,90 @@ const Store = () => {
                           className="inline-flex items-center m-2 px-6 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                           onClick={() => buyRing()}
                         >
-                          Buy your ring for 100 magic MacGuffins!
+                          Buy for 2 Arcana, 2 Scales.
                         </button>
-                        <button
-                          type="button"
-                          id="upgradeRing"
-                          className="inline-flex items-center m-2 px-6 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                          onClick={() => upgradeRing()}
-                        >
-                          Upgrade your ring for 10 magic MacGuffins!
-                        </button>
-                        <h4 className="text-white m-4">
-                          You ring is tier{' '}
-                          <span id="ringTierDisplay">{ringTier}</span> now.
-                        </h4>
                       </div>
                     </div>
                   </div>
                 </li>
+
+                {/* Ring Tier 2 */}
                 <li className="py-10 px-6 bg-gray-800 text-center rounded-lg xl:px-10 xl:text-left">
                   <div className="space-y-6 xl:space-y-10">
+                    <h4 className="text-white text-center font-medium text-xl m-4">
+                      Tier 2 Ring
+                    </h4>
                     <img
                       className="mx-auto h-40 w-40 rounded-full xl:w-56 xl:h-56"
-                      src={robe}
+                      src={ringSRCTier2}
                       alt=""
                     ></img>
                     <div className="space-y-2 xl:flex xl:items-center xl:justify-between">
                       <div className="font-medium text-lg leading-6 space-y-1">
                         <button
                           type="button"
-                          id="buyRobe"
+                          id="buyRing"
                           className="inline-flex items-center m-2 px-6 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                          onClick={() => buyRobe()}
+                          onClick={() => buyRing()}
                         >
-                          Buy your robe for 200 magic MacGuffins!
+                          Buy for 4 Arcana, 4 Scales, 2 Essence.
                         </button>
-                        <button
-                          type="button"
-                          id="upgradeRobe"
-                          className="inline-flex items-center m-2 px-6 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                          onClick={() => upgradeRobe()}
-                        >
-                          Upgrade your robe for 20 magic MacGuffins!
-                        </button>
-                        <h4 className="text-white m-4">
-                          You robe is tier{' '}
-                          <span id="robeTierDisplay">{robeTier}</span> now.
-                        </h4>
                       </div>
                     </div>
                   </div>
                 </li>
 
-                {/* More items... */}
+                {/* Cloak Tier 1 */}
+                <li className="py-10 px-6 bg-gray-800 text-center rounded-lg xl:px-10 xl:text-left">
+                  <div className="space-y-6 xl:space-y-10">
+                    <h4 className="text-white text-center font-medium text-xl m-4">
+                      Tier 1 Cloak
+                    </h4>
+                    <img
+                      className="mx-auto h-40 w-40 rounded-full xl:w-56 xl:h-56"
+                      src={cloakSRCTier1}
+                      alt=""
+                    ></img>
+                    <div className="space-y-2 xl:flex xl:items-center xl:justify-between">
+                      <div className="font-medium text-lg leading-6 space-y-1">
+                        <button
+                          type="button"
+                          id="buyCloak"
+                          className="inline-flex items-center m-2 px-6 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                          onClick={() => buyCloak()}
+                        >
+                          Buy for 2 Arcana, 2 Scales.
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </li>
+
+                {/* Cloak Tier 2 */}
+                <li className="py-10 px-6 bg-gray-800 text-center rounded-lg xl:px-10 xl:text-left">
+                  <div className="space-y-6 xl:space-y-10">
+                    <h4 className="text-white text-center font-medium text-xl m-4">
+                      Tier 2 Cloak
+                    </h4>
+                    <img
+                      className="mx-auto h-40 w-40 rounded-full xl:w-56 xl:h-56"
+                      src={cloakSRCTier2}
+                      alt=""
+                    ></img>
+                    <div className="space-y-2 xl:flex xl:items-center xl:justify-between">
+                      <div className="font-medium text-lg leading-6 space-y-1">
+                        <button
+                          type="button"
+                          id="buyCloak"
+                          className="inline-flex items-center m-2 px-6 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                          onClick={() => buyCloak()}
+                        >
+                          Buy for 4 Arcana, 4 Scales, 2 Essence.
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </li>
               </ul>
             </div>
           </div>
