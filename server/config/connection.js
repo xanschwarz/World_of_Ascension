9c87bc14b3cfd7da21f3297de113e151675944ce
Xander <xanschwarz@gmail.com>
2021-11-18 22:46:49 -0800

    Heroku deploy with mongoDB Atlas.

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/acension',
+  process.env.MONGODB_URI || 'mongodb://localhost/ascensionGame',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
