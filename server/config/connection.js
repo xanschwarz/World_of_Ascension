3b180dc69cd381df4792d352d2d0cc9a63aef1c7
aabazary <85041715+aabazary@users.noreply.github.com>
2023-04-14 22:37:23 -0700

    **Deploy Attempt**added redirects file to public folder

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,13 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/ascension',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  }
-);
+mongoose.connect(process.env.MONGODB_URI || process.env.DB_NAME, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
 module.exports = mongoose.connection;
