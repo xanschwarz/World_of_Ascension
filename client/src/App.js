ec174e00aaa5288e45dcb408d2c45c21a3d6e6a5
aabazary <85041715+aabazary@users.noreply.github.com>
2023-04-15 02:59:03 -0700

    Added URI to ApolloClient, attempting to resolve graphql issue

--- client/src/App.js
+++ client/src/App.js
@@ -1,42 +1,43 @@
-import React from 'react';
-import 'tailwindcss/tailwind.css';
+import React from "react";
+import "tailwindcss/tailwind.css";
 import {
   ApolloClient,
   InMemoryCache,
   ApolloProvider,
   createHttpLink,
-} from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Footer from './components/Footer.js';
-import NavBar from './components/NavBar.js';
-import StatsBar from './components/StatsBar.js';
-import GameContainer from './components/GameContainer';
-import Signup from './pages/Signup';
-import Login from './pages/Login';
-import Profile from './pages/Profile';
-import Home from './pages/Home';
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Footer from "./components/Footer.js";
+import NavBar from "./components/NavBar.js";
+import StatsBar from "./components/StatsBar.js";
+import GameContainer from "./components/GameContainer";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+import Home from "./pages/Home";
 
 // Construct our main GraphQL API endpoint
 const httpLink = createHttpLink({
-  uri: '/graphql',
+  uri: "/graphql",
 });
 
 // Construct request middleware that will attach the JWT token to every request as an `authorization` header
 const authLink = setContext((_, { headers }) => {
   // get the authentication token from local storage if it exists
-  const token = localStorage.getItem('id_token');
+  const token = localStorage.getItem("id_token");
   // return the headers to the context so httpLink can read them
   return {
     headers: {
       ...headers,
-      authorization: token ? `Bearer ${token}` : '',
+      authorization: token ? `Bearer ${token}` : "",
     },
   };
 });
 
 const client = new ApolloClient({
   // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+  uri: "/.netlify/functions/graphql",
   link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
@@ -49,7 +50,7 @@ function App() {
           {/* <Header /> */}
           <div
             className=" max-w-screen  bg-gray-800 p-3"
-            style={{ border: '1px dotted #1a1a1a' }}
+            style={{ border: "1px dotted #1a1a1a" }}
           >
             <StatsBar />
           </div>
